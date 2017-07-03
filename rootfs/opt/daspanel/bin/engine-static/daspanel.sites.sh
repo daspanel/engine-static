81dc6d14de3436f8623649e04b89bc5f74b21f78
Abner G Jacobsen <abner@apoana.com.br>
2017-07-03 16:23:19 -0300

    Removed dependence from daspanel/alpine-base.

--- rootfs/opt/daspanel/bin/engine-static/daspanel.sites.sh
+++ rootfs/opt/daspanel/bin/engine-static/daspanel.sites.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 content=$(wget -O- --header=Content-Type:application/json --header="Authorization: $DASPANEL_SYS_UUID" "$DASPANEL_SYS_APISERVER/sites/httpconf/$DASPANEL_SYS_HOSTNAME")
-echo "[DASPANEL-ENGINE] INFO Processing site templates for engine: $ENGINE"
+echo "[DASPANEL-$DASPANEL_CONTAINER_TYPE] INFO Processing site templates for engine: $ENGINE"
 
 # Remove all config of SITES-AVAILBLE FOR this engine
 rm /etc/caddy/sites-enabled/*
 rm /etc/caddy/sites-available/*
-echo $content
+
 # Generate new configs
-echo $content | jq -rc '.[]' | while IFS='' read site;do
-    siteuuid=$(echo "$site" | jq -r ._cuid)
-    siteenabled=$(echo "$site" | jq -r .enabled)
-    configs=$(echo $site | jq -r .configs )
-    echo $configs | jq -rc '.[]' | while IFS='' read sitecfg;do
-        siteengine=$(echo "$sitecfg" | jq -r .engine)
+echo $content | /opt/daspanel/bin/jq -rc '.[]' | while IFS='' read site;do
+    siteuuid=$(echo "$site" | /opt/daspanel/bin/jq -r ._cuid)
+    siteenabled=$(echo "$site" | /opt/daspanel/bin/jq -r .enabled)
+    configs=$(echo $site | /opt/daspanel/bin/jq -r .configs )
+    echo $configs | /opt/daspanel/bin/jq -rc '.[]' | while IFS='' read sitecfg;do
+        siteengine=$(echo "$sitecfg" | /opt/daspanel/bin/jq -r .engine)
         if [ "$siteengine" == "$ENGINE" ]; then
-            sitetype=$(echo "$sitecfg" | jq -r .sitetype)
+            sitetype=$(echo "$sitecfg" | /opt/daspanel/bin/jq -r .sitetype)
             template=""
             template1="/opt/daspanel/data/$DASPANEL_SYS_UUID/conf-templates/engine-$ENGINE/caddy/$sitetype-$siteengine.template"
             template2="/opt/daspanel/conf-templates/engine-$ENGINE/caddy/$sitetype-$siteengine.template"
@@ -25,20 +25,20 @@ echo $content | jq -rc '.[]' | while IFS='' read site;do
                     template=$template2
                 else
                     template=""
-                    echo "[DASPANEL-ENGINE] FAIL Site $siteuuid missing templates: $template1 OR $template2"
+                    echo "[DASPANEL-$DASPANEL_CONTAINER_TYPE] FAIL Site $siteuuid missing templates: $template1 OR $template2"
                 fi
             fi
             if [ ! -z "$template" ]; then
                 export SITEINFO=$site
                 export SITECFG=$sitecfg
-                echo "[DASPANEL-ENGINE] INFO Processing site $siteuuid template: $template"
-                /usr/bin/gomplate \
+                echo "[DASPANEL-$DASPANEL_CONTAINER_TYPE] INFO Processing site $siteuuid template: $template"
+                /opt/daspanel/bin/gomplate \
                     < $template \
                     >> /etc/caddy/sites-available/$siteuuid.conf
             fi
             # do your processing here
         else
-            echo "[DASPANEL-ENGINE] Ignoring sites of engine $siteengine: $0"
+            echo "[DASPANEL-$DASPANEL_CONTAINER_TYPE] Ignoring sites of engine $siteengine: $0"
         fi
     done
     if [ "$siteenabled" = true ] ; then
